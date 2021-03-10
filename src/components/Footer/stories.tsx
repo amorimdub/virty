bb3a0adc2ebc8494f2b96793b917e36e8c6189ba
Felipe Silva <amorimdub@gmail.com>
2021-03-10 04:55:00 +0000

    fix(footer): fix deployment

--- src/components/Footer/stories.tsx
+++ src/components/Footer/stories.tsx
@@ -1,9 +1,16 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Footer from '.'
+import Footer, { FooterProps } from '.'
 
 export default {
   title: 'Footer',
   component: Footer
 } as Meta
 
-export const Default: Story = () => <Footer />
+export const Default: Story<FooterProps> = (args) => <Footer {...args} />
+
+Default.args = {
+  social: {
+    facebook: 'https://facebook.com',
+    instagram: 'https://instagram.com'
+  }
+}
